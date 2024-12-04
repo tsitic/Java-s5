8a56a5ccda2a9591e87a375f6d4a4a790ea0ba5c
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-05 02:12:12 +0300

    Delete Типы данных и операторы/операторы/классы-оболочки/1.java

--- Типы данных и операторы/операторы/классы-оболочки/1.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Integer hexX = Integer.decode("0x1A");
-        System.out.println("16 based: " + hexX); // Вывод: 26
-    }
-}
