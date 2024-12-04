8a29603ec0e7af1336673c5f069bbbf12e690e97
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-05 01:17:38 +0300

    Create 5.java

---
+++ Типы данных и операторы/Базовые типы данных и литералы/5.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Пример 1: int и double - double
+        int a1 = 10;
+        double b1 = 5.5;
+        double result1 = a1 + b1;
+        System.out.println("rezult (int + double): " + result1);
+
+        // Пример 2: byte и int - int
+        byte a2 = 50;
+        int b2 = 100;
+        int result2 = a2 * b2;
+        System.out.println("rezult (byte * int): " + result2);
+    }
+}
