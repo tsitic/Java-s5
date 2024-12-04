ef82f2e1d905bea42747c1177112058dc67a6872
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-05 02:11:33 +0300

    Create 1.java

---
+++ Типы данных и операторы/операторы/классы-оболочки/1.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Integer hexX = Integer.decode("0x1A");
+        System.out.println("16 based: " + hexX); // Вывод: 26
+    }
+}
