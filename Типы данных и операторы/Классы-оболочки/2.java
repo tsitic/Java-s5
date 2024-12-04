95324e98c4415c5c9b166c13f80c09e607250e1a
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-05 02:18:19 +0300

    Create 2.java

---
+++ Типы данных и операторы/Классы-оболочки/2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Boolean bool1 = Boolean.valueOf(true);
+        Boolean bool2 = Boolean.valueOf("true");
+        boolean bool3 = Boolean.parseBoolean("true");
+        //через decode у boolean нету !
+    }
+}
