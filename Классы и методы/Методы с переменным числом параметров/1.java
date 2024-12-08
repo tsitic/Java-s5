24dfef8770b04e557069a464ef65276bca01e1dc
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 03:46:53 +0300

    Update 1.java

--- Классы и методы/Методы с переменным числом параметров/1.java
+++ Классы и методы/Методы с переменным числом параметров/1.java
@@ -1,1 +1,22 @@
+public class printNum {
+        public static void display(int... numbers) {
+        System.out.println("Num (int):");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 
+    public static void display(String... strings) {
+        System.out.println("Str:");
+        for (String str : strings) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        display(1, 2, 3, 4);               // Вызов метода с int
+        display("Привет", "Мир");          // Вызов метода с String
+    }
+}
