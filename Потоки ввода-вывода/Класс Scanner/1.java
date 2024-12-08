3f4d3998a8e293e62be75a4f887cd2d7a150d4cc
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 16:37:25 +0300

    Update 1.java

--- Потоки ввода-вывода/Класс Scanner/1.java
+++ Потоки ввода-вывода/Класс Scanner/1.java
@@ -1,1 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        try {
+            File file = new File("README.txt");
+
+            Scanner fileScanner = new Scanner(file);
+
+            // Чтение данных построчно
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                System.out.println(line); 
+            }
+
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("not found: " + e.getMessage());
+        }
+    }
+}
