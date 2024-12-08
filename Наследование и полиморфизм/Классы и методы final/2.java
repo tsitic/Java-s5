864e751bb410ca2984dd0628177bc038b6d224e9
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 04:12:47 +0300

    Create 2.java

---
+++ Наследование и полиморфизм/Классы и методы final/2.java
@@ -0,0 +1,9 @@
+final class Parent {
+    void show() {
+        System.out.println("Метод final-класса");
+    }
+}
+
+// Ошибка: нельзя наследоваться от final-класса
+// class Child extends Parent {
+// }
