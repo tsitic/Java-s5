65c67239a54dc6faf059674169005a5c9f07cb50
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 03:52:24 +0300

    Update 1.java

--- Классы и методы/Перечисления/1.java
+++ Классы и методы/Перечисления/1.java
@@ -1,1 +1,30 @@
+public enum Season {
+    WINTER("-10* С"),
+    SPRING("14* С"),
+    SUMMER("25* С"),
+    AUTUMN("14* С");
 
+    private final String tempr;
+
+    Season(String tempr) {
+        this.tempr = tempr;
+    }
+
+    public String gettempr() {
+        return tempr;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Season season = Season.WINTER;
+
+        System.out.println("Season: " + season);
+        System.out.println("t: " + season.gettempr());
+
+        // Перебор всех
+        for (Season s : Season.values()) {
+            System.out.println(s + ": " + s.gettempr());
+        }
+    }
+}
