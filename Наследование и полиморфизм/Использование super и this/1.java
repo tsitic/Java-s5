cfd6214bc694b6a02648533bbe70605300a50acf
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 04:17:14 +0300

    Update 1.java

--- Наследование и полиморфизм/Использование super и this/1.java
+++ Наследование и полиморфизм/Использование super и this/1.java
@@ -1,1 +1,38 @@
+class Parent {
+    int num;
 
+    Parent(int num) {
+        this.num = num; //Поле суперкласс
+    }
+
+    void display() {
+        System.out.println("Метод суперкласса: " + num);
+    }
+}
+
+class Child extends Parent {
+    int num;
+
+    Child(int childNum, int parentNum) {
+        super(parentNum); //Обращение к конструктору суперкласса
+        this.num = childNum; //Поле подкласса
+    }
+
+    void show() {
+        System.out.println("Поле подкласса: " + num);
+        System.out.println("Поле суперкласса: " + super.num); //Доступ к полю суперкласса
+        super.display(); //Доступ к методу суперкласса
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child(10, 20);
+        child.show();
+        /*
+        Поле подкласса: 10
+        Поле суперкласса: 20
+        Метод суперкласса: 20
+        */
+    }
+}
