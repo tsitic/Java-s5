fcc2680ae35f7532ac2e65dc2f4522f70bf79464
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 03:31:08 +0300

    Update 1.java

--- Классы и методы/Параметризованные классы/1.java
+++ Классы и методы/Параметризованные классы/1.java
@@ -1,1 +1,32 @@
+class Wrapper<T> {  //класс из примера
+    private T item;
 
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Wrapper<Integer> int1 = new Wrapper<>();
+        Wrapper<Integer> int2 = new Wrapper<>();
+        Wrapper<String> strWrapper = new Wrapper<>();
+
+        if (int1 instanceof Wrapper) {
+            System.out.println("int1 is Wrapper");
+        }
+
+        if (int2 instanceof Wrapper) {
+            System.out.println("int2 is Wrapper");
+        }
+
+        if (strWrapper instanceof Wrapper) {
+            System.out.println("strWrapper is Wrapper");
+        }
+    }
+}
