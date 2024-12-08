f9b445c1c1aca55ac7d3fa1144ee14e13bdc0162
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 16:19:33 +0300

    Update 1.java

--- Внутренние классы/Вложенные (nested) классы/1.java
+++ Внутренние классы/Вложенные (nested) классы/1.java
@@ -1,59 +1,17 @@
-/*
-Публичный внутренний класс (PublicInner) - доступен отовсюду
-Приватный внутренний класс (PrivateInner) - доступен только внутри ClassA
-Защищённый внутренний класс (ProtectedInner) - доступен внутри пакета и наследникам
-Класс с пакетным доступом (SimpInner) - доступен только внутри пакета
-*/
-
 public class ClassA {
+    // Вложенный статический класс
+    public static class NestedClass {
 
-    public class PublicInner {
-
-        public void display() {
-            System.out.println("Public inner class");
+        public void func() {
+            System.out.println("NestedClass");
         }
-    }
-
-    private class PrivateInner {
-
-        public void display() {
-            System.out.println("Private inner class");
-        }
-    }
-
-    protected class ProtectedInner {
-
-        public void display() {
-            System.out.println("Protected inner class");
-        }
-    }
-
-    class SimpInner {
-
-        public void display() {
-            System.out.println("simple inner class");
-        }
-    }
-
-    public void accessPrivateInner() {
-        PrivateInner privateInner = new PrivateInner();
-        privateInner.display();
     }
 }
 
-class Main {
+public class Main {
 
     public static void main(String[] args) {
-
-        ClassA.PublicInner publicInner = new ClassA().new PublicInner();
-        publicInner.display();
-
-        ClassA.ProtectedInner protectedInner = new ClassA().new ProtectedInner();
-        protectedInner.display();
-
-        ClassA.SimpInner packagePrivateInner = new ClassA().new SimpInner();
-        packagePrivateInner.display();
-
-        new ClassA().accessPrivateInner();
+        ClassA.NestedClass nestedObject = new ClassA.NestedClass();
+        nestedObject.func();
     }
 }
