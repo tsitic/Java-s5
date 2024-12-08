a7423ecd4996970bdb7ca2af15c5e237f70245e1
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 23:35:06 +0300

    Update 1.java

--- Функциональные интерфейсы/Методы default и static в интерфейсах/1.java
+++ Функциональные интерфейсы/Методы default и static в интерфейсах/1.java
@@ -1,1 +1,35 @@
+interface MyInterface {
+    default void defaultMethod() {
+        System.out.println("default interface");
+    }
+
+    static void staticMethod() {
+        System.out.println("static interface");
+    }
+}
+
+class A implements MyInterface {
+    //Переопределение default метода
+    @Override
+    public void defaultMethod() {
+        System.out.println("default in class");
+    }
+
+    //Дополнительный метод для вызова default метода интерфейса
+    public void callSuperDefault() {
+        MyInterface.super.defaultMethod();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A();
+        //Вызов переопределенного default метода из класса
+        obj.defaultMethod();
+        //Вызов оригинального default метода интерфейса
+        obj.callSuperDefault();
+        //Вызов статического метода интерфейса
+        MyInterface.staticMethod();
+    }
+}
 
