c3b70d7c002e78104b8cb0b39806cdb7f6830835
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 04:11:59 +0300

    Update 1.java

--- Наследование и полиморфизм/Классы и методы final/1.java
+++ Наследование и полиморфизм/Классы и методы final/1.java
@@ -1,1 +1,13 @@
+class Parent {
+    final void show() {
+        System.out.println("Final метод суперкласса");
+    }
+}
+
+class Child extends Parent {
+    // Ошибка: нельзя переопределить final метод
+    // void show() {
+    //     System.out.println("Переопределение запрещено");
+    // }
+}
 
