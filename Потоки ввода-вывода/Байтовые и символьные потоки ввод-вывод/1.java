f634d291b82cd707b78fac89ee0e2ef05d8cb229
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 17:59:15 +0300

    Update 1.java

--- Потоки ввода-вывода/Байтовые и символьные потоки ввод-вывод/1.java
+++ Потоки ввода-вывода/Байтовые и символьные потоки ввод-вывод/1.java
@@ -1,1 +1,16 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try (FileInputStream file = new FileInputStream("read.txt")) {
+            int data;
+            while ((data = file.read()) != -1) { //Чтение по байту
+                System.out.print((char) data); //Преобразование байта в символ
+            }
+        } catch (IOException e) {
+            System.out.println("error: " + e.getMessage());
+        }
+    }
+}
 
