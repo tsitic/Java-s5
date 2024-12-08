d720cec7b1f3c4b81ee369fac8565b9920a3e451
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 18:20:56 +0300

    Update 1.java

--- Строки/Форматирование информации/1.java
+++ Строки/Форматирование информации/1.java
@@ -1,1 +1,16 @@
+import java.util.Formatter;
 
+public class Main {
+
+    public static void main(String[] args) {
+        try (Formatter formatter = new Formatter()) {
+            formatter.format("logic: %b%n", true);
+            formatter.format("Char: %c%n", 'T');
+            formatter.format("Integer: %d%n", 42);
+            formatter.format("Float: %.2f%n", 3.14159);
+            formatter.format("Str: %s%n", "Hello");
+            //Вывод форматированного текста
+            System.out.println(formatter);
+        }
+    }
+}
