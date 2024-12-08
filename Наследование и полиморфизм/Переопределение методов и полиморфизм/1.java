eed5d04d4d73b5d259e02c19af8b16048b4b91de
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-08 14:54:28 +0300

    Update 1.java

--- Наследование и полиморфизм/Переопределение методов и полиморфизм/1.java
+++ Наследование и полиморфизм/Переопределение методов и полиморфизм/1.java
@@ -1,1 +1,25 @@
+class Sum {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+
+        System.out.println(sum.add(2, 3));  //5
+        System.out.println(sum.add(1, 2, 3)); //6
+        System.out.println(sum.add(1.5, 2.5));  //4.0
+    }
+}
 
