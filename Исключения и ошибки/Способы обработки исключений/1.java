d358cf3e4ff9e4b72579f42f8ee930729e91d84f
Tsitic <77049931+tsitic@users.noreply.github.com>
2024-12-09 00:10:12 +0300

    Update 1.java

--- Исключения и ошибки/Способы обработки исключений/1.java
+++ Исключения и ошибки/Способы обработки исключений/1.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Первое исключение - будет перехвачено
+            String str = null;
+            str.length();
+        } catch (NullPointerException e) {
+            System.out.println("Перехвачено исключение: " + e.getMessage());
+        }
 
+        // Второе исключение - приведет к аварийной остановке
+        int[] arr = new int[5];
+        arr[10] = 100; // ArrayIndexOutOfBoundsException
+    }
+}
